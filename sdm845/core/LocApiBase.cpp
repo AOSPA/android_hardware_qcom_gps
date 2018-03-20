35a95e0a9bc9aeab1bb1847180babda2da5fbf90
Saurabh Srivastava <ssrivast@codeaurora.org>
2018-03-20 17:53:20 +0530

    FR 46082 - SUPL Network Setup Improvements
    Adding enhancement to ATL messages to allow
    sending bearer type and apn type mask values.
    
    CRs-Fixed: 2209227
    Change-Id: I09a294946610f26de4760a0fba6f13083f8752f3

--- sdm845/core/LocApiBase.cpp
+++ sdm845/core/LocApiBase.cpp
@@ -349,10 +349,10 @@ void LocApiBase::requestLocation()
     TO_1ST_HANDLING_LOCADAPTERS(mLocAdapters[i]->requestLocation());
 }
 
-void LocApiBase::requestATL(int connHandle, LocAGpsType agps_type)
+void LocApiBase::requestATL(int connHandle, LocAGpsType agps_type, LocApnTypeMask mask)
 {
     // loop through adapters, and deliver to the first handling adapter.
-    TO_1ST_HANDLING_LOCADAPTERS(mLocAdapters[i]->requestATL(connHandle, agps_type));
+    TO_1ST_HANDLING_LOCADAPTERS(mLocAdapters[i]->requestATL(connHandle, agps_type, mask));
 }
 
 void LocApiBase::releaseATL(int connHandle)
@@ -361,10 +361,10 @@ void LocApiBase::releaseATL(int connHandle)
     TO_1ST_HANDLING_LOCADAPTERS(mLocAdapters[i]->releaseATL(connHandle));
 }
 
-void LocApiBase::requestSuplES(int connHandle)
+void LocApiBase::requestSuplES(int connHandle, LocApnTypeMask mask)
 {
     // loop through adapters, and deliver to the first handling adapter.
-    TO_1ST_HANDLING_LOCADAPTERS(mLocAdapters[i]->requestSuplES(connHandle));
+    TO_1ST_HANDLING_LOCADAPTERS(mLocAdapters[i]->requestSuplES(connHandle, mask));
 }
 
 void LocApiBase::reportDataCallOpened()
@@ -454,7 +454,8 @@ DEFAULT_IMPL(LOC_API_ADAPTER_ERR_SUCCESS)
 
 enum loc_api_adapter_err LocApiBase::
    atlOpenStatus(int /*handle*/, int /*is_succ*/, char* /*apn*/,
-                 AGpsBearerType /*bear*/, LocAGpsType /*agpsType*/)
+                 AGpsBearerType /*bear*/, LocAGpsType /*agpsType*/,
+                 LocApnTypeMask /*mask*/)
 DEFAULT_IMPL(LOC_API_ADAPTER_ERR_SUCCESS)
 
 enum loc_api_adapter_err LocApiBase::
