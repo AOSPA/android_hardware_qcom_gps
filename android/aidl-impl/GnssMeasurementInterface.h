35dc9c39f8dc5161673c42a62bf0825e76a11782
Mike Cailean <mcailean@codeaurora.org>
2021-02-22 09:49:48 -0800

    AIDL Implementation for measurements for Android S
    Change-Id: I03dcbbd15fde240d291178c25a09eeaacbca9c12
    CRs-fixed: 2883356

--- android/aidl-impl/GnssMeasurementInterface.h
+++ android/aidl-impl/GnssMeasurementInterface.h
@@ -19,6 +19,19 @@
  */
 
 #include <aidl/android/hardware/gnss/BnGnssMeasurementInterface.h>
+#include <aidl/android/hardware/gnss/BnGnssMeasurementCallback.h>
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <LocationAPIClientBase.h>
+#include <gps_extended_c.h>
+
+using aidl::android::hardware::gnss::ElapsedRealtime;
+using aidl::android::hardware::gnss::GnssClock;
+using aidl::android::hardware::gnss::GnssData;
+using aidl::android::hardware::gnss::GnssMeasurement;
+using aidl::android::hardware::gnss::GnssSignalType;
+using aidl::android::hardware::gnss::GnssConstellationType;
 
 namespace android {
 namespace hardware {
@@ -29,14 +42,44 @@ namespace implementation {
 using ::aidl::android::hardware::gnss::BnGnssMeasurementInterface;
 using ::aidl::android::hardware::gnss::IGnssMeasurementCallback;
 
-struct GnssMeasurementInterface : public BnGnssMeasurementInterface {
+struct GnssMeasurementInterface : public BnGnssMeasurementInterface, public LocationAPIClientBase {
 public:
-    ::ndk::ScopedAStatus setCallback(const std::shared_ptr<IGnssMeasurementCallback>& in_callback,
+    GnssMeasurementInterface();
+    ~GnssMeasurementInterface() {}
+    ::ndk::ScopedAStatus setCallback(
+            const std::shared_ptr<IGnssMeasurementCallback>& in_callback,
             bool in_enableFullTracking, bool in_enableCorrVecOutputs) override;
     ::ndk::ScopedAStatus close() override;
 
+    // callbacks we are interested in
+    void onGnssMeasurementsCb(GnssMeasurementsNotification gnssMeasurementsNotification) final;
+
 private:
-    std::shared_ptr<IGnssMeasurementCallback> mGnssMeasurementCb = nullptr;
+    std::shared_ptr<IGnssMeasurementCallback> mGnssMeasurementCbIface = nullptr;
+    // Synchronization lock for mGnssMeasurementCbIface
+    mutable std::mutex mMutex;
+    AIBinder_DeathRecipient* mDeathRecipient;
+    bool mTracking;
+
+    static void gnssMeasurementDied(void* cookie);
+    void startTracking(GnssPowerMode powerMode = GNSS_POWER_MODE_INVALID,
+                       uint32_t timeBetweenMeasurement = GPS_DEFAULT_FIX_INTERVAL_MS);
+    void convertGnssData(GnssMeasurementsNotification& in, GnssData& out);
+    void convertGnssMeasurement(GnssMeasurementsData& in, GnssMeasurement& out);
+    void convertGnssFlags(GnssMeasurementsData& in, GnssMeasurement& out);
+    static void convertGnssSvId(GnssMeasurementsData& in, int& out);
+    void convertGnssSignalType(GnssMeasurementsData& in, GnssSignalType& out);
+    static void convertGnssConstellationType(GnssSvType& in, GnssConstellationType& out);
+    void convertGnssMeasurementsCodeType(GnssMeasurementsCodeType& inCodeType,
+                                         char* inOtherCodeTypeName, GnssSignalType& out);
+    void convertGnssState(GnssMeasurementsData& in, GnssMeasurement& out);
+    void convertGnssAccumulatedDeltaRangeState(GnssMeasurementsData& in, GnssMeasurement& out);
+    void convertGnssMultipathIndicator(GnssMeasurementsData& in, GnssMeasurement& out);
+    void convertGnssSatellitePvt(GnssMeasurementsData& in, GnssMeasurement& out);
+    void convertGnssClock(GnssMeasurementsClock& in, GnssClock& out);
+    void convertElapsedRealtimeNanos(GnssMeasurementsNotification& in,
+                                     ElapsedRealtime& elapsedRealtime);
+    void printGnssData(GnssData& data);
 };
 
 }  // namespace implementation
