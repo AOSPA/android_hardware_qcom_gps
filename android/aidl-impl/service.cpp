0c9dd119705933457421f4f84eecc57055fbec8c
Kevin Tang <zhikait@codeaurora.org>
2021-04-20 11:53:07 -0700

    copyright header fix
    Change-Id: I765272f039e13b61a81d35f9b626343ea260aa99
    CRs-Fixed: 2927925

--- android/aidl-impl/service.cpp
+++ android/aidl-impl/service.cpp
@@ -1,7 +1,21 @@
 /*
- * Copyright (c) 2021 Qualcomm Technologies, Inc.
- * All Rights Reserved.
- * Confidential and Proprietary - Qualcomm Technologies, Inc.
+ * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ * Not a Contribution
+ */
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2_0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2_0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <aidl/android/hardware/gnss/IGnss.h>
