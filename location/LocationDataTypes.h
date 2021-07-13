61fad42f00e1baaad02e5b352bda92867bbfbc39
yingjiew <yingjiewang@codeaurora.org>
2021-07-13 15:40:15 +0800

    Set bit mask for QualityType/TechMask in HAL
    Need to set bit flag for QualityType/TechMask in Location
    before report to SDK;
    Correct FIXED_QUALITY_TYPE to include both
    LOC_NAV_MASK_RTK_CORRECTION and
    LOC_NAV_MASK_RTK_FIXED_CORRECTION;
    Update HEPE usage.
    
    Change-Id: Icb66d91104d8e70dc1523449490cef1307c9abf5
    CRs-Fixed: 2989862

--- location/LocationDataTypes.h
+++ location/LocationDataTypes.h
@@ -79,6 +79,8 @@ typedef enum {
     LOCATION_HAS_SPOOF_MASK            = (1<<8), // location has valid spoof mask
     LOCATION_HAS_ELAPSED_REAL_TIME     = (1<<9), // location has valid elapsed real time
     LOCATION_HAS_CONFORMITY_INDEX_BIT  = (1<<10), // location has valid conformity index
+    LOCATION_HAS_QUALITY_TYPE_BIT      = (1<<11), // location has valid quality type
+    LOCATION_HAS_TECH_MASK_BIT         = (1<<12), // location has valid tech mask
 } LocationFlagsBits;
 
 typedef uint16_t LocationTechnologyMask;
