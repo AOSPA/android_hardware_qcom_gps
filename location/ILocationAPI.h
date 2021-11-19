e381435251b17235c3d623bd62bd295b62adaaf2
Bhavna Sharma <quic_sbhavna@quicinc.com>
2021-11-19 13:50:10 -0800

    Necessary changes for redesign of LocationClientApiImpl
    Include some new API's and data structures to ILocationAPI,
    which are present on LocationClientApiImpl.
    Add call to updateCallbacks when creating LocationAPI as well
    to work for HIDL clients.
    
    CRs-Fixed: 3090998
    
    Change-Id: I04da2cbbc553ed4cf262f7c5f97749e872bfe309

--- location/ILocationAPI.h
+++ location/ILocationAPI.h
@@ -26,6 +26,42 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
+
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted (subject to the limitations in the
+disclaimer below) provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef ILOCATIONAPI_H
 #define ILOCATIONAPI_H
 
@@ -179,6 +215,24 @@ public:
     /** @brief stopNetworkLocation stops the ongoing tracking session for
        network location request */
     virtual void stopNetworkLocation(trackingCallback* callback) {}
+
+    /** @brief Get energy consumed info of modem GNSS engine */
+    virtual void getGnssEnergyConsumed(gnssEnergyConsumedCallback gnssEnergyConsumedCb,
+            responseCallback responseCb) {}
+
+    /** @brief Retrieve single-shot terrestrial position using the set of
+        specified terrestrial technologies. */
+    virtual void getSingleTerrestrialPosition(uint32_t timeoutMsec,
+            TerrestrialTechMask techMask,
+            float horQoS, trackingCallback terrestrialPositionCallback,
+            responseCallback responseCb) {}
+
+    /** @brief  Register/update listener to receive location system info
+        that are not tied with positioning session, e.g.: next leap
+        second event. */
+    virtual void updateLocationSystemInfoListener(
+            locationSystemInfoCallback locationSystemInfoCb,
+            responseCallback responseCb) {}
 };
 
 class ILocationControlAPI
