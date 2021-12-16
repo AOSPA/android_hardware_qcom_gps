83ea77a66c999f1f14ac793a524c262a4b9c9ed4
Naresh Munagala <quic_nareshm@quicinc.com>
2021-12-16 16:06:29 +0530

    loc-nmea: check for valid utc time
    always check for valid utc time before generating the
    NMEA strings.
    
    Change-Id: I174b7a42f5ecaa88d228104dd2a77e7fd54f976a
    CRs-Fixed: 3095685

--- utils/loc_nmea.cpp
+++ utils/loc_nmea.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2022, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1407,8 +1407,9 @@ void loc_nmea_generate_pos(const UlpLocation &location,
         sv_cache_info.navic_used_mask =
                 locationExtended.gnss_sv_used_ids.navic_sv_used_ids_mask;
     }
-
-    if (generate_nmea) {
+    // Generate valid NMEA strings only when utc Time stamp is set
+    // Output empty nmea sentence if utctime is zero
+    if (generate_nmea && (0 != utcPosTimestamp)) {
         char talker[3] = {'G', 'P', '\0'};
         uint32_t svUsedCount = 0;
         uint32_t count = 0;
