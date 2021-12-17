c6b4203e2e3cb85640ffb1d68c4ccff8691f8639
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2021-12-17 14:23:02 -0800

    Elapsed time: fix potential issue due to unsigned arithmetic
    There is an issue that substraction of two unsigned number can
    cause overflow/underflow. Force integer to signed 64 bit before
    doing arithmetic operation
    
    Change-Id: I94d6f398ee95e2e045e88da2820fc39a9efa3099
    CRs-fixed: 3096831

--- core/LocApiBase.cpp
+++ core/LocApiBase.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2014, 2016-2021 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2014, 2016-2022 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1103,16 +1103,20 @@ bool ElapsedRealtimeEstimator::getElapsedRealtimeForGpsTime(
     // We have valid association
     if (mTimePairMeasReport.gpsTime.gpsWeek != 0) {
         timePair = mTimePairMeasReport;
-        LOC_LOGv("user meas time association");
+        LOC_LOGv("use meas time association");
     } else if (mTimePairPVTReport.gpsTime.gpsWeek != 0) {
-        LOC_LOGv("user PVT time association");
+        LOC_LOGv("use PVT time association");
         timePair = mTimePairPVTReport;
     } else {
         return false;
     }
 
-    gpsTimeDiffMsec = (gpsTimeAtOrigin.gpsWeek - timePair.gpsTime.gpsWeek) * MSEC_IN_ONE_WEEK +
-                       (gpsTimeAtOrigin.gpsTimeOfWeekMs - timePair.gpsTime.gpsTimeOfWeekMs);
+    int64_t originMsec = (int64_t)gpsTimeAtOrigin.gpsWeek * (int64_t)MSEC_IN_ONE_WEEK +
+                         (int64_t)gpsTimeAtOrigin.gpsTimeOfWeekMs;
+    int64_t timePairMsec = (int64_t)timePair.gpsTime.gpsWeek * (int64_t)MSEC_IN_ONE_WEEK +
+                            (int64_t)timePair.gpsTime.gpsTimeOfWeekMs;
+    gpsTimeDiffMsec = originMsec - timePairMsec;
+
     qtimerNsecAtOrigin = timePair.qtimerTick * 10000/192 + gpsTimeDiffMsec * 1000000;
 
     clock_gettime(CLOCK_BOOTTIME, &curBootTime);
