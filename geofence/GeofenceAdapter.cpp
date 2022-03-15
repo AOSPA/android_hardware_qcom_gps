426564a46f84d7de1915c83dc63177d3b54304dd
yingjiew <quic_yingjiew@quicinc.com>
2022-03-15 18:05:39 +0800

    Inject DwellTypeMask and Confidence for Geofence
    When adding geofence from location SDK API, DwellTypeMask is
    dropped and confidence is hardcoded as HIGH regardless of input.
    
    Change-Id: I4dd0f5f684bab6b52991b2b77b41aaa7d5e7afea
    CRs-Fixed: 3150859

--- geofence/GeofenceAdapter.cpp
+++ geofence/GeofenceAdapter.cpp
@@ -26,6 +26,41 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
+
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted (subject to the limitations in the
+disclaimer below) provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #define LOG_TAG "LocSvc_GeofenceAdapter"
 
 #include <GeofenceAdapter.h>
@@ -160,7 +195,8 @@ GeofenceAdapter::restartGeofences()
         GeofenceOption options = {sizeof(GeofenceOption),
                                    object.breachMask,
                                    object.responsiveness,
-                                   object.dwellTime};
+                                   object.dwellTime,
+                                   object.confidence};
         GeofenceInfo info = {sizeof(GeofenceInfo),
                              object.latitude,
                              object.longitude,
@@ -657,6 +693,7 @@ GeofenceAdapter::saveGeofenceItem(LocationAPI* client, uint32_t clientId, uint32
                              options.breachTypeMask,
                              options.responsiveness,
                              options.dwellTime,
+                             options.confidence,
                              info.latitude,
                              info.longitude,
                              info.radius,
@@ -723,6 +760,7 @@ GeofenceAdapter::modifyGeofenceItem(uint32_t hwId, const GeofenceOption& options
         it->second.breachMask = options.breachTypeMask;
         it->second.responsiveness = options.responsiveness;
         it->second.dwellTime = options.dwellTime;
+        it->second.confidence = options.confidence;
         dump();
     } else {
         LOC_LOGE("%s]: geofence item to modify not found. hwId %u", __func__, hwId);
