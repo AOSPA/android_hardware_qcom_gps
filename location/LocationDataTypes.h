8595febfbaf21ae2512bfd60bbe9b2a6529f13f6
Yan Huang <quic_huang@quicinc.com>
2022-11-15 23:32:19 -0800

    Gnss at the tunnel entrace
    LOCATION_TECHNOLOGY_PROPAGATED mask means
    position is calculated from the cached measurements.
    Use this mask to indicate the inside tunnel PVT.
    
    Change-Id: I9314cba09ea40e415091f46f9c27fd6a581560df
    CRs-Fixed: 3386956

--- location/LocationDataTypes.h
+++ location/LocationDataTypes.h
@@ -146,6 +146,7 @@ typedef enum {
     LOCATION_TECHNOLOGY_DGNSS_BIT                    = (1<<11), // DGNSS
     LOCATION_TECHNOLOGY_HYBRID_ALE_BIT               = (1<<12), // HYBRID using ALE POS
     LOCATION_TECHNOLOGY_PDR_BIT                      = (1<<13), // PED mode
+    LOCATION_TECHNOLOGY_PROPAGATED_BIT               = (1<<14), //using cached measures
 } LocationTechnologyBits;
 
 typedef uint32_t LocationSpoofMask;
