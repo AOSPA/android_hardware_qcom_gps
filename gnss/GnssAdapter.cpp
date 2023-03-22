CodeLinaro-mirror/femto5g_platform_hardware_qcom_gps

Remove race condition while mSupportNfwControl read
Race condition in reading mSupportNfwControl
set by MsgGnssUpdateConfig. MsgControlNfwLocationAccess
end up reading stale value. Making sure MsgControlNfwLocationAccess
see the latest value of mSupportNfwControl. moved the
mSupportNfwControl inside the msg task proc.

Change-Id: Ib487d5e0beb6a0a25ca08b8356fbb2abc4b37051
CRs-fixed: 3436929

gnss/GnssAdapter.cpp
@@ -5233,26 +5233,26 @@ GnssAdapter::nfwControlCommand(bool enable) {
             mApi(api),
             mEnable(enable) {}
         inline virtual void proc() const {
-            GnssConfigGpsLock gpsLock;
+            if (mAdapter.mSupportNfwControl) {
+                GnssConfigGpsLock gpsLock;
 
-            gpsLock = ContextBase::mGps_conf.GPS_LOCK;
-            if (mEnable) {
-                gpsLock &= ~GNSS_CONFIG_GPS_LOCK_NI;
+                gpsLock = ContextBase::mGps_conf.GPS_LOCK;
+                if (mEnable) {
+                    gpsLock &= ~GNSS_CONFIG_GPS_LOCK_NI;
+                } else {
+                    gpsLock |= GNSS_CONFIG_GPS_LOCK_NI;
+                }
+                ContextBase::mGps_conf.GPS_LOCK = gpsLock;
+                mApi.sendMsg(new LocApiMsg([&mApi = mApi, gpsLock]() {
+                    mApi.setGpsLockSync((GnssConfigGpsLock)gpsLock);
+                }));
             } else {
-                gpsLock |= GNSS_CONFIG_GPS_LOCK_NI;
+                LOC_LOGw("NFW control is not supported, do not use this for NFW status");
             }
-            ContextBase::mGps_conf.GPS_LOCK = gpsLock;
-            mApi.sendMsg(new LocApiMsg([&mApi = mApi, gpsLock]() {
-                mApi.setGpsLockSync((GnssConfigGpsLock)gpsLock);
-            }));
         }
     };
 
-    if (mSupportNfwControl) {
-        sendMsg(new MsgControlNfwLocationAccess(*this, *mLocApi, enable));
-    } else {
-        LOC_LOGw("NFW control is not supported, do not use this for NFW");
-    }
+    sendMsg(new MsgControlNfwLocationAccess(*this, *mLocApi, enable));
 }
 
 // Set tunc constrained mode, use 0 session id to indicate

AOSPA/android_hardware_qcom_gps

gnss/GnssAdapter.cpp
@@ -6699,27 +6699,28 @@ GnssAdapter::nfwControlCommand(std::vector<std::string>& enabledNfws) {
                 return;
             }
 
-            GnssConfigGpsLock gpsLock;
+            if (mAdapter.mSupportNfwControl) {
+                GnssConfigGpsLock gpsLock;
 
-            uint32_t nfwControlBits;
-            nfwControlBits = mAdapter.getNfwControlBits(mEnabledNfws);
-            gpsLock = ContextBase::mGps_conf.GPS_LOCK;
-            gpsLock &= GNSS_CONFIG_GPS_LOCK_MO;
-            gpsLock |= nfwControlBits;
-            ContextBase::mGps_conf.GPS_LOCK = gpsLock;
+                uint32_t nfwControlBits;
+                nfwControlBits = mAdapter.getNfwControlBits(mEnabledNfws);
+                gpsLock = ContextBase::mGps_conf.GPS_LOCK;
+                gpsLock &= GNSS_CONFIG_GPS_LOCK_MO;
+                gpsLock |= nfwControlBits;
+                ContextBase::mGps_conf.GPS_LOCK = gpsLock;
 
-            LOC_LOGv("gpsLock = 0x%X nfwControlBits = 0x%X", gpsLock, nfwControlBits);
-            mApi.sendMsg(new LocApiMsg([&mApi = mApi, gpsLock]() {
-                         mApi.setGpsLockSync((GnssConfigGpsLock)gpsLock);
-            }));
+                LOC_LOGv("gpsLock = 0x%X nfwControlBits = 0x%X", gpsLock, nfwControlBits);
+                mApi.sendMsg(new LocApiMsg([&mApi = mApi, gpsLock]() {
+                             mApi.setGpsLockSync((GnssConfigGpsLock)gpsLock);
+                }));
+            } else {
+                LOC_LOGw("NFW control is not supported, do not use this for NFW status");
+            }
         }
     };
 
-    if (mSupportNfwControl) {
-        sendMsg(new MsgControlNfwLocationAccess(*this, *mLocApi, enabledNfws));
-    } else {
-        LOC_LOGw("NFW control is not supported, do not use this for NFW");
-    }
+    sendMsg(new MsgControlNfwLocationAccess(*this, *mLocApi, enabledNfws));
+
 }
 
 // Set tunc constrained mode, use 0 session id to indicate